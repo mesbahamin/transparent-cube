c4b56e9dc758124337ab888e27f87516dcb5fd47
Amin Mesbah <dev@aminmesbah.com>
2019-04-27 22:02:57 -0700

    Move glfw Windows deps into libs/glfw_windows/

--- build.bat
+++ build.bat
@@ -1,15 +1,40 @@
 @echo off
 
-set CFLAGS=-std=c99 -nostdlib -Ilib -I../include/glfw/include -Wall -Wextra -Wshadow -Wswitch-enum -Wno-unused-parameter -Wno-missing-braces
-set LDFLAGS=-luser32 -lgdi32 -lwinmm -lopengl32 -lshell32
-set RELCFLAGS=-O2 -Os
+set CFLAGS= ^
+    -std=c99 ^
+    -nostdlib ^
+    -D_CRT_SECURE_NO_WARNINGS ^
+    -Ilib ^
+    -Ilib/glfw_windows/include ^
+    -Wall ^
+    -Wextra ^
+    -Wshadow ^
+    -Wswitch-enum ^
+    -Wno-unused-parameter ^
+    -Wno-missing-braces
 
-IF NOT EXIST ".\build" mkdir ".\build"
-clang %CFLAGS% %RELCFLAGS%^
- src\platform_windows.c^
- src\game.c^
- src\glad.c^
- src\glmth.c^
- src\shader.c^
- ..\include\glfw\lib-vc2015\glfw3.lib^
- -o .\build\quaternion-demo.exe %LDFLAGS%
+set LDFLAGS= ^
+    -luser32 ^
+    -lgdi32 ^
+    -lwinmm ^
+    -lopengl32 ^
+    -lshell32 ^
+    -llib/glfw_windows/lib-vc2019/glfw3.lib
+
+set RELCFLAGS= ^
+    -O2 ^
+    -Os
+
+IF NOT EXIST ".\out" mkdir ".\out"
+IF NOT EXIST ".\out\release" mkdir ".\out\release"
+
+clang ^
+    %CFLAGS% ^
+    %RELCFLAGS% ^
+    src\platform_windows.c ^
+    src\game.c ^
+    src\glad.c ^
+    src\glmth.c ^
+    src\shader.c ^
+    -o .\out\release\transparent-cube.exe ^
+    %LDFLAGS%
